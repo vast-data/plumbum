ae4dba38003ce6334e631601195b1747167517d9
Shlomi Romano <shlomi@vastdata.com>
2025-04-15 13:52:19 +0300

    plumbum/ssh_machine: Add ipv6 support to scp commands

--- plumbum/machines/ssh_machine.py
+++ plumbum/machines/ssh_machine.py
@@ -1,4 +1,5 @@
 import warnings
+import ipaddress
 
 from plumbum.commands import ProcessExecutionError, shquote
 from plumbum.lib import IS_WIN32
@@ -32,6 +33,12 @@ class SshTunnel:
         """Closes(terminates) the tunnel"""
         self._session.close()
 
+def is_ipv6_address(addr: str) -> bool:
+    try:
+        ipaddress.IPv6Address(addr)
+        return True
+    except ValueError:
+        return False
 
 class SshMachine(BaseRemoteMachine):
     """
@@ -109,6 +116,9 @@ class SshMachine(BaseRemoteMachine):
             self._fqhost = f"{user}@{host}"
         else:
             self._fqhost = host
+        # if the host is an IPv6 address, we need to wrap it in brackets for scp command (upload / download functions)
+        scp_host = f'[{self.host}]' if not self.host.startswith('[') and is_ipv6_address(self.host) else self.host
+        self.scp_fqhost = f"{user}@{scp_host}" if user else scp_host
         if port:
             ssh_args.extend(["-p", str(port)])
             scp_args.extend(["-P", str(port)])
@@ -307,7 +317,7 @@ class SshMachine(BaseRemoteMachine):
         if IS_WIN32:
             src = self._translate_drive_letter(src)
             dst = self._translate_drive_letter(dst)
-        self._scp_command(f"{self._fqhost}:{shquote(src)}", dst)
+        self._scp_command(f"{self.scp_fqhost}:{shquote(src)}", dst)
 
     def upload(self, src, dst):
         if isinstance(src, RemotePath):
@@ -319,7 +329,7 @@ class SshMachine(BaseRemoteMachine):
         if IS_WIN32:
             src = self._translate_drive_letter(src)
             dst = self._translate_drive_letter(dst)
-        self._scp_command(src, f"{self._fqhost}:{shquote(dst)}")
+        self._scp_command(src, f"{self.scp_fqhost}:{shquote(dst)}")
 
 
 class PuttyMachine(SshMachine):
